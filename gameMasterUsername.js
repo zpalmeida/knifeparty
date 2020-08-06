5970bd1711823f48adb8ee042c4f869a5a7fbe79
Paulo Carvalho <pmccarvalho12@gmail.com>
2020-08-06 14:41:57 +0100

    change in gameMasterUsername html&js

---
+++ gameMasterUsername.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    $("#username").click(submitUsername);
+
+});
+
+    function submitUsername(){
+
+        console.log("HELLOO!!!!");
+
+        // perform an ajax http get request
+        $.ajax({
+            url: '',
+            type: 'POST',
+            data: JSON.stringify({
+                "userName":$('#username').val(),
+            }),
+            async: true,
+            contentType: 'application/json',
+            success: test,
+            error: errorCallback
+
+    });
+}
+
+    function errorCallback(){
+        console.log("ERRRROOORRR!!");
+    }
+
+    function test(){
+        console.log("success!!")
+    }
+
