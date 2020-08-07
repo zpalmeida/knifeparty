0adcf4f82673c1d19d3277c68b84626f20cf78e9
Paulo Carvalho <pmccarvalho12@gmail.com>
2020-08-07 06:55:03 +0100

    objects.htlm

--- objects.js
+++ objects.js
@@ -3,40 +3,108 @@ var crimeSceneArray = [];
 
 $(document).ready(function() {
 
-    $("#whippedcream").click(function() {
+    $("#axe").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#ball").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#banana").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#belt").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#bike").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#gamecontroller").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#ice").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#key").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#knife").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#lamp").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#lighter").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#lipstick").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#liquor").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#nail").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#pen").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#pillow").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#scissors").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#skate").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#soap").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#strawberry").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#sweet").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#tissues").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#tvremote").click(function() {
+        objectInitializer("whippingcream");
+    });
+
+    $("#vase").click(function() {
         objectInitializer("whippingcream");
     });
 
     $("#waterglass").click(function() {
-        objectInitializer("waterglass");
+        objectInitializer("whippingcream");
     });
 
-    $("#vase").click(function() {
-        objectInitializer("vase");
-    });
-
-    $("#tvremote").click(function() {
-        objectInitializer("tvremote");
-    });
-
-    $("#tissues").click(function() {
-        objectInitializer("tissues");
-    });
-
-    $("#sweet").click(function() {
-        objectInitializer("sweet");
-    });
-
-    $("#strawberry").click(function() {
-        objectInitializer("strawberry");
-    });
-
-    $("#soap").click(function() {
-        objectInitializer("soap");
-    });
-
-    $("#tvremote").click(function() {
-        objectInitializer("tvremote");
+    $("#whippedcream").click(function() {
+        objectInitializer("whippingcream");
     });
 
 });
@@ -45,7 +113,7 @@ function objectInitializer(str){
 
     console.log("I am here!"+str);
     objectArray.push(str);
-    var row = '<td>Selected Weapon: '+str+'</td> ';
+    var row = '<td>'+str+'</td> ';
 
     $(".objNames").append(row);
 
