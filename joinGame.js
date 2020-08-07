57101492cf520b2154f97e74bf1cb9a02d677ece
Paulo Carvalho <pmccarvalho12@gmail.com>
2020-08-07 01:33:19 +0100

    90% done

--- joinGame.js
+++ joinGame.js
@@ -1,16 +1,38 @@
-var playerService =
+var usernamePlayer;
+
 
 $(document).ready(function() {
 
     $("#joingame").click(function() {
         userJoinGame();
+    });
+
+    $("#kill").click(function() {
+        killVictim();
 
     });
 
+  $("#rigged").click(function() {
+        riggedByPredadator();
+
+    });
+
+  $("#startgame").click(function(){
+      startGame();
+  })  
 
 });
 
- function userJoinGame(){
+function startGame(){
+    $.ajax({
+        url: 'https://killergameac.herokuapp.com/api/player/start-game',
+        async: true,
+        success: goToGame,
+        error: errorNoBlackList
+    });
+}
+
+function userJoinGame(){
 
     console.log("HELLOO!!!!");
 
@@ -24,13 +46,48 @@ $(document).ready(function() {
         }),
         async: true,
         contentType: 'application/json',
-        success: test,
-        error: errorCallback
+        success: loadingParameters($('#username').val()),
+        error: errorUserJoin
 
     });
 }
 
-function errorCallback(){
+function loadingParameters(username){
+    console.log("Sucess!");
+    usernamePlayer = username;
+    getBlacklistFunction();
+}
+
+function getBlacklistFunction(){
+
+    // perform an ajax http get request
+    $.ajax({
+        url: 'https://killergameac.herokuapp.com/api/blacklist/'+ usernamePlayer,
+        async: true,
+        success: goToGame,
+        error: errorNoBlackList
+    });
+}
+
+function goToGame(response){
+    console.log("Success. Blacklist have arrived");
+
+    var row = '<tr><td>' + 
+                response.weapon + '</td><td>' + 
+                response.crimeScene + '</td><td>' + 
+                response.victim + '</td></tr>';
+
+    $(row).appendTo('#blacklist-table');
+
+}
+
+function errorNoBlackList(){
+    console.log("Waiting!!");
+    setTimeout(function(){ alert("I am waiting.."); }, 5000);
+    getBlacklistFunction();
+}
+
+function errorUserJoin(){
     console.log("ERRRROOORRR!!");
 }
 
